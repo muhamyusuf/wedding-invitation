56ea0485b524b9f58ae18d3a2ef8e929f0f36232
Wupxy <muhammad.122140193@student.itera.ac.id>
2023-12-27 09:52:02 +0700

    section: add opening invitation and create components

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+        domains: ['i.pinimg.com','wallpapers.com'],
+    },
+}
 
 module.exports = nextConfig
