fffe94e117f40aff4880aee6d89e00c9491c6cc1
Wupxy <muhammad.122140193@student.itera.ac.id>
2024-01-09 09:42:03 +0700

    revisi lovetory

--- src/components/LoveStory.tsx
+++ src/components/LoveStory.tsx
@@ -9,14 +9,12 @@ const palatino = localFont({
 export default function LoveStory() {
  return (
   <main className="bg-[url('/assets/background-lovestory.png')] bg-cover bg-no-repeat bg-right border-y-2">
-   <MaxWidthWrapper className="flex flex-col md:flex-row relative min-h-screen items-center justify-around gap-10 py-10 px-2 md:py-10 md:px-10 lg:px-20 lg:py-20">
-    <div className="text-black flex-1">
-     <h2 className="text-[32px] font-bold font-serif text-center">
-      Love Story
-     </h2>
+   <MaxWidthWrapper className="flex flex-col md:flex-row relative min-h-screen items-center justify-around gap-10 py-10 px-2 md:py-10 md:px-10 lg:px-20 lg:py-20 font-serif">
+    <div className="text-white flex-1">
+     <h2 className="text-[32px] font-bold text-center">Love Story</h2>
 
      <div
-      className={`flex flex-col gap-10 backdrop-blur-md p-5 rounded-lg border font-serif`}
+      className={`flex flex-col gap-10 backdrop-blur-lg p-5 rounded-lg border border-white/30`}
      >
       <div>
        <h3 className="font-bold text-[24px]">&#x2022; First Meet</h3>
@@ -51,15 +49,15 @@ export default function LoveStory() {
      <div>
       <Image
        className="object-cover object-center"
-       src="/assets/rizky-dinda-textwedding.png"
-       alt="rizky-dinda-textwedding"
+       src="/assets/overlay-lovestory.png"
+       alt="overlay-lovestory"
        width={2212}
        height={620}
        quality={100}
       />
      </div>
 
-     <div className="text-black h-full backdrop-blur-md p-5 rounded-lg border font-serif">
+     <div className="text-white text-center h-full backdrop-blur-lg p-5 rounded-lg border border-white/30 font-serif">
       What counts in making a happy marriage is not so much how compatible you
       are, but how you deal with incompatibility. A great marriage is not when
       the perfect couple comes together. It is when an imperfect couple learns
